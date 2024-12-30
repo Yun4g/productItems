6adc522f1fb688a56ae19dfdae8fc7a7298d3774
Yun4g <delightvincent487@gmail.com>
2024-12-31 00:22:10 +0100

    fixed favourite section head color

--- src/App.jsx
+++ src/App.jsx
@@ -151,7 +151,7 @@ function App() {
       <aside className={`border-2 w-full sm:w-1/2 h-full md:w-2/5 lg:w-1/4 z-40 fixed top-0 right-0 backdrop-blur-sm bg-white/30 p-6 overflow-y-scroll transition-transform duration-300 ease-in-out ${displayItem ? 'translate-x-0' : 'translate-x-full'}`}>
         <section className=' h-[400px]'>
           <div className='flex justify-between items-center'>
-            <h1 className='text-2xl font-bold text-white'>
+            <h1 className='text-2xl font-bold text-gray-900'>
               Favourite item
             </h1>
             <div onClick={hideAside}>
