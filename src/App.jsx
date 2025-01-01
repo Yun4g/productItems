5f2f8fc2a0da1e30e3c432a1e8f00c181c2b9555
Yun4g <delightvincent487@gmail.com>
2025-01-01 18:50:50 +0100

    added product count

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ function App() {
     return storedFavorites;
   });
 
-  const [displayItem, setdisplayItem] = useState(()=>{
-      const storedData = JSON.parse(localStorage.getItem("displayState"));
-        return  storedData !== null ? storedData : false
+  const [displayItem, setdisplayItem] = useState(() => {
+    const storedData = JSON.parse(localStorage.getItem("displayState"));
+    return storedData !== null ? storedData : false
   });
   const [selectedItemId, setSelectedItemId] = useState(null);
-
-
   const [pending, setPending] = useState(false);
+  const [productCount, setProductCount] = useState(0);
+  
 
 
 
@@ -38,23 +38,29 @@ function App() {
   }
 
 
- 
 
 
 
-  const hideMSG = ()=>{
+
+  const hideMSG = () => {
     setSelectedItemId(null)
-  }  
+  }
 
   useEffect(() => {
-   
+
     localStorage.setItem("displayState", JSON.stringify(displayItem))
-    localStorage.setItem("favoriteItems", JSON.stringify(favoriteItems) )
 
-  }, [displayItem, favoriteItems] )
+  }, [displayItem])
+
+
 
   useEffect(() => {
-    
+    localStorage.setItem("favoriteItems", JSON.stringify(favoriteItems));
+  }, [favoriteItems]);
+
+
+  useEffect(() => {
+
     const products = async () => {
 
       try {
@@ -62,62 +68,74 @@ function App() {
         setPending(true)
         const response = await fetch('https://fakestoreapi.com/products');
         const result = await response.json()
-        
-          if (result.ok) {
-             setPending(false)
-            
-          } else {
-            setPending(false)
-          }
-          setItemsData(result);
+
+        if (result.ok) {
+          setPending(false)
+
+        } else {
+          setPending(false)
+        }
+        setItemsData(result);
       } catch (error) {
         console.log(error)
       }
-   
+
 
     }
-    
+
     products();
   }, [])
 
 
-
+  console.log(favoriteItems)
 
 
   const AddToFav = (item) => {
     if (!favoriteItems.find((fav) => fav.id === item.id)) {
-      setFavouriteItems([...favoriteItems, item])
+      const updatedItem = [...favoriteItems, item]
+      setFavouriteItems(updatedItem)
+    
     }
-
-    setdisplayItem(true)
-    console.log(favoriteItems)
+  
   }
 
-  console.log(itemsData)
+
+
+  useEffect(() => {
+    setProductCount(favoriteItems.length)
+  }, [favoriteItems])
+
+
+
   return (
 
     <section className=' px-4 md:px-6 lg:px-12'>
-     {pending && <DescriptionMsg/>}  
-          
+      {pending && <DescriptionMsg />}
+
       <div className={` fixed top-0 w-full h-full  justify-center items-center ${selectedItemId === null ? "hidden" : "flex"
         }`}>
         <div className={` bg-gray-900 text-slate-50 descriptive-message transition-opacity duration-300 ease-in-out rounded-xl p-6   md:w-1/3  `}>
 
-          
-        <div onClick={hideMSG} className=' absolute top-2 right-2  border w-8 h-8 flex rounded-full justify-center items-center' >
-          <i className="fa-solid fa-x cursor-pointer text-white"></i>
-        </div>
+
+          <div onClick={hideMSG} className=' absolute top-2 right-2  border w-8 h-8 flex rounded-full justify-center items-center' >
+            <i className="fa-solid fa-x cursor-pointer text-white"></i>
+          </div>
         </div>
 
 
       </div>
 
 
-      <header className='h-32 flex w-full     justify-between  items-center'>
-        <div className=' h-28 flex backdrop-blur-sm bg-white/30 w-full   rounded-b-xl  justify-between  items-center'>
-          <h1 className=' md:text-5xl lg:text-7xl w-11/12 flex  justify-center items-center text font-bold text-center text-gray-800'>shopping items</h1>
+      <header className='   h-32 flex w-full     justify-between  items-center'>
+        <div className=' fixed top-0  h-28 flex flex-col md:flex-row backdrop-blur-sm bg-white/30   w-[96%]     rounded-b-xl justify-center gap-2  md:justify-between  items-center'>
+          <h1 className=' text-3xl lg:text-7xl w-11/12 flex  justify-center items-center text font-bold text-center text-gray-800'>shopping items</h1>
 
-          <button onClick={toggleAside} className=' h-12 w-full md:w-80    rounded-lg bg-gray-900 me-7 md:me-12 sm:text-sm   text-gray-100 font-bold' type="button">Favourite Item</button>
+
+          <button onClick={toggleAside} className=' h-12  w-72 md:w-44 flex justify-center items-center gap-2 md:gap-3  rounded-lg bg-gray-900 me-7 md:me-12 sm:text-sm   text-gray-100 font-bold' type="button">Favourite Item
+            <span className='  flex justify-center items-center bg-slate-100 text-slate-900   w-6  h-6 rounded-full '>
+              <p>{productCount}</p>
+            </span>
+          </button>
         </div>
       </header>
 
@@ -135,9 +153,14 @@ function App() {
 
 
                 <div className='w-full flex items-center justify-center h-1/3'>
-                  <button onClick={() => AddToFav(item)} className=' h-12 w-2/3 rounded-lg bg-gray-900 text-gray-100 font-bold mt-2' type="button">
-                    Add to Favourite
-                  </button>
+
+                  
+                      <button onClick={() => AddToFav(item)} className=' h-12 w-1/2 rounded-lg bg-gray-900 text-gray-100 font-bold mt-2' type="button">
+                        Add to Favourite
+                      </button> 
+
+                   
+
                 </div>
               </div>
             </div>
@@ -158,7 +181,7 @@ function App() {
               <i className="fa-solid fa-x cursor-pointer text-gray-700"></i>
             </div>
           </div>
-          <FavouriteItemSection favoriteItems={favoriteItems}/>
+          <FavouriteItemSection favoriteItems={favoriteItems} />
         </section>
       </aside>
 
